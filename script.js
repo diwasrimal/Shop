90c35daa7fd1a4ab2baec62f5101c65fbea30781
Diwas_lufy <84910758+diwasrimal@users.noreply.github.com>
2022-04-25 01:47:43 +0545

    add shop.html with css

---
+++ script.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', () => {
+    document.getElementById('proceed-btn').addEventListener('click', () => {
+        document.getElementById('form').style.visibility = 'visible';
+    })
+});
